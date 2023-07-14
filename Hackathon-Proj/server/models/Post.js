bb30cc9d3c3a4657847e4f9a6ea674fe35d5baf6
Object Oriented ProgrammingObject Oriented ProgrammingObject Oriented ProgrammingObject Oriented Programming <back>
2023-07-14 11:09:40 -0600

    Added Posts Post and Get

---
+++ Hackathon-Proj/server/models/Post.js
@@ -0,0 +1,31 @@
+import { Schema } from "mongoose";
+
+export const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 20
+  },
+  postImg: {
+    type: String,
+    required: true,
+    maxlength: 300
+  },
+  description: {
+    type: String,
+    maxlength: 100
+  },
+  category: {
+    type: String,
+    enum: ['Flat Bread', 'Chicago', 'New York', 'Stuffed Crust', 'Italian', 'Thin Crust', 'Thick Crust', 'Pan', 'Cauliflower', 'Unknown'],
+    default: 'Unknown'
+  },
+  profileId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Account'
+  }
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true }
+})
