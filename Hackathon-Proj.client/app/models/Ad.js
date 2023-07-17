4b17c131d74dc8bdf5609b1dd520dcf5fc135bbc
Ewan Stubblefield-Allen <ewan2004@yahoo.com>
2023-07-16 21:43:53 -0700

    Styled Ad

--- Hackathon-Proj.client/app/models/Ad.js
+++ Hackathon-Proj.client/app/models/Ad.js
@@ -1,31 +1,32 @@
 export class Ad {
   static get AdTemplate() {
-    return `
-    <div class="col-10 tomato-glass text-white my-3 px-0">
+    return /*HTML*/`
+    <div class="col-10 tomato-glass text-white border border-dark my-3 px-0">
       <div class="row">
-        <div class="col-12 col-md-3">
-          <img class="img-fluid post-style w-100" src="" alt="Hot Pizza in your area">
+        <div class="col-12 col-md-5 d-flex align-items-center p-3">
+          <button class="fs-2 px-3 py-4 ad-btn text-break" type="button">START DOWNLOAD</button>
+          <i class="mdi mdi-menu-left text-success fs-1"></i>
         </div>
 
-        <div class="col-12 col-md-7 p-3 text-center text-md-start">
+        <div class="col-12 col-md-5 p-3 text-center text-md-start">
           <div class="d-md-flex justify-content-between">
             <div class="d-flex justify-content-center justify-md-content-start align-items-center">
-              <p class="fs-1">
+              <p class="fs-4">
                 Hot Pizzas In Your Area
               </p>
             </div>
           </div>
-          <p class="category">Call Now: 1-800-Hot-Piza</p>
-          <p class="text-break"></p>
+          <p>3 Easy Steps:</p>
+          <ol class="text-break">
+            <li>Click "Download"</li>
+            <li>Download on our website</li>
+            <li>Enjoy</li>
+          </ol>
         </div>
 
-        <div class="col-12 col-md-2 d-flex flex-md-column justify-content-between py-2 px-4">
-          <div class="d-flex flex-row-reverse flex-md-row align-items-center justify-content-end">
-            <p class="p-1">Ad</p>
-          </div>
-          <div class="d-flex justify-content-end align-items-center">
-
-          </div>
+        <div class="col-12 col-md-2 d-flex py-2 px-4 justify-content-end">
+          <i class="mdi mdi-information-outline ad-icon p-1 me-1 fs-5"></i>
+          <i class="mdi mdi-close ad-icon p-1 fs-5"></i>
         </div>
       </div>
     </div>`
